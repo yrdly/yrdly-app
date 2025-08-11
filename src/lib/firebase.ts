3633ddc32ccbe6e2382139cade065ff5cfd9073f
Caleb <feranmioyelowo@gmail.com>
2025-08-11 23:09:02 +0000

    Error Firebase: Error (auth/configuration-not-found).

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,16 +4,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
+  projectId: "yrdly-coyig",
+  appId: "1:166095077947:web:2f5285213610bbd893521e",
+  storageBucket: "yrdly-coyig.firebasestorage.app",
   apiKey: "AIzaSyCOE_L6g9c3J6jhcajVzNkC4flEao4Av2I",
   authDomain: "yrdly-coyig.firebaseapp.com",
-  projectId: "yrdly-coyig",
-  storageBucket: "yrdly-coyig.firebasestorage.app",
-  messagingSenderId: "166095077947",
-  appId: "1:166095077947:web:2f5285213610bbd893521e",
+  messagingSenderId: "166095077947"
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
