b28783546bab5a13ae4a8ceb11ad9853c4d0f589
Caleb <feranmioyelowo@gmail.com>
2025-08-12 14:26:11 +0000

    when i upload an image and post, only the text shows but the image does

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -19,6 +20,8 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
