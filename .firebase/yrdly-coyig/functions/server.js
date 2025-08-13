c3bae1b972670ab9009886bbb9bd73fd47d7f21d
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-08-13 21:22:42 +0100

    fix: Remove large files from git history

--- .firebase/yrdly-coyig/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssryrdlycoyig = onRequest({"id":"yrdly-backend"}, (req, res) => server.then(it => it.handle(req, res)));
-  
