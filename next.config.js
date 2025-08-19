700d21ac798f579a5cfd2da17914686c8597426f
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-08-19 16:05:09 +0100

    fixed finally

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     // Required for Capacitor to work with Next.js's static export
     images: {
         unoptimized: true
