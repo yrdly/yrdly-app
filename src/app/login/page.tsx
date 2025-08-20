130e3fac26ac847a2845b0c99e82de228612bd2c
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-08-20 02:21:10 +0100

    stupid typo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -215,7 +215,7 @@ export default function LoginPage() {
                                       <DialogHeader>
                                           <DialogTitle>Reset Password</DialogTitle>
                                           <DialogDescription>
-                                              Enter your email address and we'll send you a link to reset your password.
+                                              Enter your email address and we&apos;ll send you a link to reset your password.
                                           </DialogDescription>
                                       </DialogHeader>
                                       <div className="py-4">
